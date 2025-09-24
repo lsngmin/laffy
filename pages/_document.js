de835b893b064ac713c03ac58f1a825c049ba5bc
sngmin <lsgmin@naver.com>
2025-09-24 16:35:25 +0900

    비디오 썸네일, 경로 변경

--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,26 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-GYM11LKQML"
-            strategy="afterInteractive"
-          />
-          <Script
-            id="gtag-init"
-            strategy="afterInteractive"
+          {/* Google Analytics */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-GYM11LKQML"></script>
+          <script
             dangerouslySetInnerHTML={{
               __html: `
                 window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
+                function gtag(){dataLayer.push(arguments);} 
                 gtag('js', new Date());
                 gtag('config', 'G-GYM11LKQML');
-              `
+              `,
             }}
           />
-          {/** Ads disabled: monetag meta removed */}
+
+          {/* Monetag - bootstrap IIFE and external tag */}
+          <script data-cfasync="false" src="/ads/monetag-inline.js"></script>
+          <script src="//x7i0.com/tag.min.js" data-zone="9903140" data-cfasync="false" async></script>
         </Head>
         <body>
           <Main />
@@ -33,4 +31,3 @@ class MyDocument extends Document {
   }
 }
 
-export default MyDocument;
